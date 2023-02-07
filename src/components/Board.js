ccec719830680d288fc0745ab0f66a3ec625b43d
Elaine <sosohn@gmail.com>
2023-02-07 12:28:16 -0800

    remove Board.css

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,9 @@
-import './Board.css'
 const Board = ({id, title, owner, status, cards, selected, onSelect}) => {
     const selectBoardHandler = () => {
         onSelect(id);
     }
     return (          
-        <button className="board-title" onClick={selectBoardHandler}>{title}</button>
+        <button onClick={selectBoardHandler}>{title}</button>
     );
 };
 
