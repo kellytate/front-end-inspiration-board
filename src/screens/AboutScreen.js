4b155e4905fafa5f32d989f2744511caac44e11b
kelly <kelly.tate.dev@gmail.com>
2023-02-09 15:10:52 -0800

    Added About details

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const AboutScreen = () => {
-  return <div>AboutScreen</div>;
+  return (
+  <div>
+    <h3>D-E-C-K-Y Collaborators</h3>
+    <ul>
+        <li>Dalia</li>
+        <li>Elaine</li>
+        <li>Cindy</li>
+        <li>Kelly</li>
+        <li>Yvette</li>
+    </ul>
+  </div>
+  );
 };
 
 export default AboutScreen;
